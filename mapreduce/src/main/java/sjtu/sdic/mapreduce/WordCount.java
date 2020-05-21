0796c99047ce97b100656dbfb3cab12f57e6851b
dianaaaa <qianxingyueyx6@sjtu.edu.cn>
2020-05-21 23:00:05 +0800

    finished part II

--- mapreduce/src/main/java/sjtu/sdic/mapreduce/WordCount.java
+++ mapreduce/src/main/java/sjtu/sdic/mapreduce/WordCount.java
@@ -18,12 +18,29 @@ public class WordCount {
 
     public static List<KeyValue> mapFunc(String file, String value) {
         // Your code here (Part II)
-       return null;
+        List<KeyValue> res = new ArrayList<>();
+        String regex = "[a-zA-Z0-9]+";
+        Pattern p = Pattern.compile(regex);
+        Matcher  ma = p.matcher(value);
+        List<String> words = new ArrayList<>();
+        while (ma.find()) {
+            //String word = ma.group().toLowerCase();
+            String word = ma.group();
+            //System.out.println(word);
+            words.add(word);
+        }
+        for (int i = 0; i < words.size(); i++) {
+            KeyValue kv = new KeyValue(words.get(i), "");
+            res.add(kv);
+        }
+        return res;
     }
 
     public static String reduceFunc(String key, String[] values) {
         // Your code here (Part II)
-        return null;
+        String res = String.valueOf(values.length);
+        //System.out.println("reduce res: " + res);
+        return res;
     }
 
     public static void main(String[] args) {
